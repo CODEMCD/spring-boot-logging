43426539d13c42afe4740d5b8d7953e71db0d313
CODEMCD <psbum1609@gmail.com>
2020-11-06 21:13:16 +0900

    init project

---
+++ src/main/java/com/codemcd/springbootloggingsystem/SpringBootLoggingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.codemcd.springbootloggingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoggingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLoggingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codemcd/springbootloggingsystem/SpringBootLoggingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemcd.springbootloggingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoggingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
